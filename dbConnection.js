8f96155f5debbfe2dcecdd957fe2f70e34c46329
Agrim-ani <agrimani.125@gmail.com>
2023-09-08 20:34:44 +0530

    DB uri param changed

--- dbConnection.js
+++ dbConnection.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv').config()
 mongoose.set('strictQuery', true)
 const connectDb = async()=>{
     try {
-        const connect = await mongoose.connect(process.env.DB_CONNECTION)
+        const connect = await mongoose.connect("mongodb+srv://admin:admin@cluster0.wsrqfyy.mongodb.net/?retryWrites=true&w=majority")
         console.log("DATABASE is CONNECTED")
     } catch (error) {
         console.log(error)
